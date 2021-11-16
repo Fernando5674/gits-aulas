47782a8b39d2f6f00f162c3ffce0761ff2d39846
Aluno IFSC <IFSC@CDR-718107.cacador.ifsc.edu.br>
2021-11-16 15:33:03 -0300

    Impressão de dados com Scanner

---
+++ src/githubfernando/fidelis/GithubFernandoFidelis.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubfernando.fidelis;
+
+/**
+ *
+ * @author IFSC
+ */
+public class GithubFernandoFidelis {
+
+    /**
+     * @param args the command line arguments
+     */ 
+    public static void main(String[] args) {
+     
+ Scanner leia = new Scanner(System.in);
+ String.out.println("Entre seu nome");
+ String nome = leia.nextLine();
+ System.out.println(nome+"bem vindo ao GitHub");
+ 
+        
+    }
+    
+}
